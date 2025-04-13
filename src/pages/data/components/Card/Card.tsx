0aaf4371b47f6bc39ee5a639d6231667314c251c
JuanesEspinosa <juanes.espinosa01@gmail.com>
2025-04-13 00:12:15 -0500

    ajustes en data

--- src/pages/data/components/Card/Card.tsx
+++ src/pages/data/components/Card/Card.tsx
@@ -9,8 +9,8 @@ const Card = ({option, setOption}: {option: string, setOption: (option: string) 
     return (
         <div className="grid grid-cols-4 gap-10">
             {dataCard.map((dataCard) => (
-            <div key={dataCard.title} onClick={() => setOption(dataCard.title)} className="flex flex-col justify-center items-center cursor-pointer w-[120px] h-[120px] bg-neutral-200 rounded-[10px]  hover:bg-neutral-300">
-                <div className="text-6xl text-neutral-700">{dataCard.icon}</div>
+            <div key={dataCard.title} onClick={() => setOption(dataCard.title)} className="flex flex-col justify-center items-center cursor-pointer w-[100px] h-[100px] bg-neutral-200 rounded-[10px]  hover:bg-neutral-300">
+                <div className="text-4xl text-neutral-700">{dataCard.icon}</div>
                     <div className="text-lg font-semibold text-neutral-700">{dataCard.title}</div>
                 </div>
             ))}
